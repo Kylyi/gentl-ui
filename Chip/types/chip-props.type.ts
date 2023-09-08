154a517141caddf6cfd54015aead118dcb8f8bc3
Kylian, Jakub <jacob.kylian@gmail.com>
2023-09-08 16:47:35 +0200

    feat: Add copy to chip

--- Chip/types/chip-props.type.ts
+++ Chip/types/chip-props.type.ts
@@ -1,9 +1,33 @@
 import { INavigation } from '~~/components/Button/types/btn-props.type'
 
 export interface IChipProps extends INavigation {
+  /**
+   * Whether the chip has the CopyBtn
+   */
+  hasCopy?: boolean
+
+  /**
+   * Whether the chip has the button to remove it
+   */
   hasRemove?: boolean
+
+  /**
+   * The chip's icon
+   */
   icon?: string
+
+  /**
+   * The chip's label
+   */
   label?: string | number
+
+  /**
+   * Whether the chip has the ripple effect
+   */
   ripple?: boolean
+
+  /**
+   * Whether the chip centered
+   */
   center?: boolean
 }
