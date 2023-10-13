5bfba8c586c0526f6263da1ab68159cdd926e807
Kylian, Jakub <jacob.kylian@gmail.com>
2023-10-13 22:31:59 +0200

    feat: list

--- List/functions/useList.ts
+++ List/functions/useList.ts
@@ -643,6 +643,14 @@ export function useList(
 
   onKeyStroke(['ArrowUp', 'ArrowDown', 'Enter'], handleKey)
 
+  whenever(
+    () => props.search,
+    _search => {
+      console.log(_search)
+      search.value = _search
+    }
+  )
+
   return {
     arr,
     hoveredIdx,
