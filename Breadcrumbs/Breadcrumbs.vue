32ef6e07b1d829ebfebbddb8ce9385fdc6d68611
David Flek <david.flek@backtech.cz>
2023-10-26 15:39:03 +0200

    feat: accurate width calculations on client side.

--- Breadcrumbs/Breadcrumbs.vue
+++ Breadcrumbs/Breadcrumbs.vue
@@ -14,43 +14,106 @@ const breadcrumbs = computed(() => {
     },
     ...breadcrumbsInjected.value,
   ]
-    .flatMap(breadcrumb => [breadcrumb, 'splitter'])
-    .slice(0, -1)
+  // .flatMap(breadcrumb => [breadcrumb, 'splitter'])
+  // .slice(0, -1)
+})
+
+// Refs for breadcrumb container and items
+const breadcrumbsWrapperEl = ref<HTMLElement>()
+const breadcrumbEls = ref<(Element | ComponentPublicInstance | null)[]>([])
+const slotRightEl = ref<HTMLElement>()
+const slotAppendEl = ref<HTMLElement>()
+
+// Widths
+const { width: containerWidth } = useElementSize(breadcrumbsWrapperEl)
+const { width: slotRightWidth } = useElementSize(slotRightEl)
+const { width: slotAppendWidth } = useElementSize(slotAppendEl)
+const slotsWidth = computed(() => {
+  return floor(slotRightWidth.value + slotAppendWidth.value)
+})
+const breadcrumbsAvaiableWidth = computed(() => {
+  return containerWidth.value - slotsWidth.value
+})
+
+// Calculate visible breadcrumbs
+const isBreadcrumbVisibleByIndex = ref<boolean[]>([])
+const breadcrumbsWidthByIndex = ref<number[]>([])
+
+onMounted(() => {
+  setTimeout(() => {
+    for (const [index, breadcrumb] of breadcrumbs.value.entries()) {
+      isBreadcrumbVisibleByIndex.value[index] = true
+      const breadcrumbEl = breadcrumbEls.value[
+        breadcrumbs.value.indexOf(breadcrumb)
+      ] as HTMLElement
+
+      if (breadcrumbEl) {
+        breadcrumbsWidthByIndex.value[index] =
+          breadcrumbEl.offsetWidth + (index === 0 ? 0 : 4) // 4px for gap
+      }
+    }
+  }, 1)
 })
 </script>
 
 <template>
-  <div class="breadcrumbs-wrapper">
+  {{ containerWidth }}
+  <br />
+  slotsWidth: {{ slotsWidth }}
+  <br />
+  breadCrumbsAvaiableWidth: {{ breadcrumbsAvaiableWidth }}
+  <br />
+  {{ breadcrumbsWidthByIndex }}
+  {{ breadcrumbsWidthByIndex.reduce((a, b) => a + b, 0) }}
+  <br />
+  visible breadcrumbs by index {{ isBreadcrumbVisibleByIndex }}
+
+  <div
+    ref="breadcrumbsWrapperEl"
+    class="breadcrumbs-wrapper"
+  >
     <div class="breadcrumbs">
       <template
-        v-for="breadcrumb in breadcrumbs"
+        v-for="(breadcrumb, index) in breadcrumbs"
         :key="breadcrumb.to"
       >
-        <!-- Chevron -->
-        <span
-          v-if="typeof breadcrumb === 'string'"
-          :class="BUTTON_PRESET.CHEVRON_RIGHT.icon"
-          class="breadcrumb-item"
-        />
+        <div
+          :ref="
+            el => {
+              breadcrumbEls[index] = el
+            }
+          "
+          flex
+          flex-col
+          items-center
+        >
+          <!-- Chevron -->
+          <span
+            v-if="index !== 0"
+            :class="BUTTON_PRESET.CHEVRON_RIGHT.icon"
+            class="breadcrumb-item"
+          />
 
-        <Btn
-          v-else
-          v-bind="breadcrumb"
-          no-active-link
-          color="!dark !dark:light"
-          size="sm"
-          class="breadcrumb-item"
-          :class="{
-            'is-last': breadcrumb === breadcrumbs[breadcrumbs.length - 1],
-          }"
-          no-uppercase
-        />
+          <Btn
+            v-bind="breadcrumb"
+            no-active-link
+            color="!dark !dark:light"
+            size="sm"
+            class="breadcrumb-item"
+            :class="{
+              'is-last': breadcrumb === breadcrumbs[breadcrumbs.length - 1],
+            }"
+            no-uppercase
+          />
+        </div>
       </template>
-
-      <slot name="append" />
+      <div ref="slotAppendEl">
+        <slot name="append" />
+      </div>
     </div>
-
-    <slot name="right" />
+    <div ref="slotRightEl">
+      <slot name="right" />
+    </div>
   </div>
 </template>
 
