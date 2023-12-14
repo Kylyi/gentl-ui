d1468f1639c83e2bed728b93234006386daec246
Kylian, Jakub <jacob.kylian@gmail.com>
2023-12-14 19:22:16 +0100

    fix: types

--- Crud/CrudBtnRestore.vue
+++ Crud/CrudBtnRestore.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-// TYPES
-import type { IBtnProps } from '~/components/Button/types/btn-props.type'
+// Types
+import type { RouteLocationRaw } from '#vue-router'
 import type { ICrudBtnProps } from '~/components/Crud/types/crud-btn-props.type'
 
 type IProps = ICrudBtnProps & {
-  to?: IBtnProps['to']
+  to?: RouteLocationRaw
 }
 
 withDefaults(defineProps<IProps>(), {
