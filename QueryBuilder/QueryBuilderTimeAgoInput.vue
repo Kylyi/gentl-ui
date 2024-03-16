7be9731a08fb0e72ae6946cf93ee3e5c6abece3e
Kylian, Jakub <jacob.kylian@gmail.com>
2024-03-16 18:59:08 +0100

    chore: input layout

--- QueryBuilder/QueryBuilderTimeAgoInput.vue
+++ QueryBuilder/QueryBuilderTimeAgoInput.vue
@@ -103,7 +103,7 @@ function setUnit(unitShortName: string) {
     ref="inputEl"
     :model-value="agoValue.value"
     size="sm"
-    layout="label-inside"
+    layout="regular"
     class="qb-item__content-value"
     @update:model-value="agoValue = { value: $event }"
   >
