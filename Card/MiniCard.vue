fd6f4d65ae236dd8ff0a0c0d9d4255da4428dfef
Kylian, Jakub <jacob.kylian@gmail.com>
2024-03-26 17:08:37 +0100

    fix: minicard empty value string default

--- Card/MiniCard.vue
+++ Card/MiniCard.vue
@@ -3,7 +3,7 @@
 import { type IMiniCardProps } from '~/components/Card/types/mini-card-props.type'
 
 // Functions
-import { useValueFormatterUtils } from '~/components/ValueFormatter/functions/useValueForamtterUtils'
+import { useValueFormatterUtils } from '~/components/ValueFormatter/functions/useValueFormatterUtils'
 
 const props = withDefaults(defineProps<IMiniCardProps>(), {
   previousValue: undefined,
