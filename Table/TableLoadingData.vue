5146eb4f29a98929337ce94532b9e253d343533a
Kylian, Jakub <jacob.kylian@gmail.com>
2024-03-28 19:04:59 +0100

    chore: visuals

--- Table/TableLoadingData.vue
+++ Table/TableLoadingData.vue
@@ -14,20 +14,28 @@ const hasRows = computed(() => props.rows && props.rows?.length > 0)
     :class="{ 'is-absolute': hasRows }"
     data-cy="loading-table"
   >
-    <LoaderInline
-      size="sm"
+    <div
+      class="flex flex-center"
+      relative
       min-w="70"
       backdrop="blur-sm"
-    />
+    >
+      <LoaderInline
+        size="sm"
+        bg="white dark:darker"
+        rounded="full"
+        h="8"
+      />
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .loading {
-  --apply: flex flex-center;
+  --apply: flex flex-center z-1;
 
   &.is-absolute {
-    --apply: absolute inset-inline-0 bottom-0;
+    --apply: absolute inset-inline-0 bottom-0 h-10;
   }
 }
 </style>
