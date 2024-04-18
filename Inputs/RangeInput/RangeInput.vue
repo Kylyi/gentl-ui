2feea0f61ea39e37aa09ab6c4de3ac14c550c5dc
Kylian, Jakub <jacob.kylian@gmail.com>
2024-04-18 13:21:53 +0200

    feat: range preparation

---
+++ Inputs/RangeInput/RangeInput.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+type IProps = {
+  modelValue?: number
+}
+
+defineProps<IProps>()
+defineEmits<{
+  (e: 'update:modelValue', value: number): void
+}>()
+
+// Layout
+const model = defineModel<number>()
+</script>
+
+<template>
+  <input
+    v-model.number="model"
+    type="range"
+  />
+</template>
