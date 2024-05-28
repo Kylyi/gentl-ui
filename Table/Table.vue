20156e7c96ebf33e4cc20c49e4af8e19570b7e6f
Kylian, Jakub <jacob.kylian@gmail.com>
2024-05-28 15:17:53 +0200

    chore: add prop

--- Table/Table.vue
+++ Table/Table.vue
@@ -98,6 +98,7 @@ defineExpose({
       totalRowsRef: Ref<number | undefined>
       tableRowHeight: typeof tableRowHeight.value
       scrollerEl: typeof scrollerEl.value
+      refreshData: () => void
     }) => void
   ) => {
     fnc({
@@ -106,6 +107,7 @@ defineExpose({
       totalRowsRef: totalRows,
       tableRowHeight: tableRowHeight.value,
       scrollerEl: scrollerEl.value,
+      refreshData: () => refreshData(true)
     })
   },
 })
