3fda437ce06541e455a92760efdb822810431855
David Flek <davidflek@backtech.cz>
2024-05-29 16:28:38 +0200

    fix: put back a fallback comparator

--- Table/models/table-column.model.ts
+++ Table/models/table-column.model.ts
@@ -526,7 +526,7 @@ export class TableColumn<T = IItem> implements IItemBase<T> {
     // @ts-expect-error - defaultComparators aren't supported for all dataTypes
     this.comparators = col.comparators ?? defaultComparatorsByDataType[this.dataType]
     // @ts-expect-error - defaultComparators aren't supported for all dataTypes
-    this.comparator = col.comparator ?? defaultComparatorByDataType[this.dataType]
+    this.comparator = col.comparator ?? defaultComparatorByDataType[this.dataType] ?? this.comparator
     this.extraComparators = col.extraComparators
     this.noFilterSort = col.noFilterSort ?? false
     this.filterFormat = col.filterFormat
